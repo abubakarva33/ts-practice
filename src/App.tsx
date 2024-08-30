e3b93668e67c3fc8580767978f015becfce13883
Abubakar <abubakarsiddikl.info@gmail.com>
2024-08-30 23:42:19 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
 
 const increment = ( orpFn:(x: number, y:number)=>number, x:number, y:number)=> {return orpFn(x,y)}
 console.log(increment((x,y)=>x+y,10,20));
+console.log(increment((x,y)=>x*y,10,20));
 
 
   // JavaScript Example
