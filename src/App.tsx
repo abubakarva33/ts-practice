f9e0734a828ccc9aa57bd9c3004dabb4f6a49775
Abubakar <abubakarsiddikl.info@gmail.com>
2024-08-31 23:22:05 +0600

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
+
 import './App.css'
 
 function App() {
 
-  type StudentType = {
-    name: string,
-    age: number
-  }
-
-  type employeeType = {
-    employeeId: number,
-    department: string
-  }
-
-
-
-
-  // JavaScript Example
-const person:StudentType = {
-  name: "John",
-  age: 30
-};
-
-const employee:employeeType = {
-  employeeId: 1234,
-  department: "HR"
-};
-
-const personEmployee = {
-  ...person,
-  ...employee
-};
-
-console.log(personEmployee);
-  
 
   return (
     <>
