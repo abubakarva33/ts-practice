5549f3f5fbe2854f05a6083db6b619a09aac0582
Abubakar <abubakarsiddikl.info@gmail.com>
2024-09-01 23:52:44 +0600

    new chnage

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
           className="flex flex-col justify-center items-center w-[650px]  "
           style={{ border: "1px", borderColor: "black", borderStyle: "solid", margin: "70px" }}
         >
+          new change
+         </div>
         </div>
       </>
       )
